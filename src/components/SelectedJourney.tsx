347ea21ca81af314058c574380d3e9979ce65c67
Andeskjerf <predgd@pm.me>
2024-08-21 13:12:06 +0200

    nicer animation

--- src/components/SelectedJourney.tsx
+++ src/components/SelectedJourney.tsx
@@ -4,33 +4,40 @@ import './SelectedJourney.css'
 
 interface SelectedJourneyProps {
   journey: Journey
-	removeJourney: Function
+  removeJourney: Function
 }
 
-export default function SelectedJourney({ journey, removeJourney }: SelectedJourneyProps) {
+export default function SelectedJourney({
+  journey,
+  removeJourney,
+}: SelectedJourneyProps) {
   const fromTime = timeFromDateTime(journey.departure).substring(0, 5)
   const toTime = timeFromDateTime(journey.arrival).substring(0, 5)
 
   function toggleRemoveButton(show: boolean) {
-		const elem = document.getElementById(`selected-delete-${journey.id}`)
-		if (elem && show) {
-			elem.classList.add('show')
-		} else if (elem && !show) {
-			elem.classList.remove('show')
-		}
-	}
+    const elem = document.getElementById(`selected-delete-${journey.id}`)
+    if (elem && show) {
+      elem.classList.add('show')
+    } else if (elem && !show) {
+      elem.classList.remove('show')
+    }
+  }
 
   return (
     <>
       <div
         onMouseEnter={() => toggleRemoveButton(true)}
-				onMouseLeave={() => toggleRemoveButton(false)}
+        onMouseLeave={() => toggleRemoveButton(false)}
         className='white-bg round-both mr-12 mt-12 p-10 text-black flex flex-column elevation no-select relative'
       >
         <div className='text-medium'>{fromTime}</div>
         <div className='text-medium'>{toTime}</div>
         <div className='text-bold'>{journey.legs[0].line.name}</div>
-        <div onClick={() => removeJourney(journey)} id={`selected-delete-${journey.id}`} className='white-bg absolute x-remove elevation pointer-cursor'>
+        <div
+          onClick={() => removeJourney(journey)}
+          id={`selected-delete-${journey.id}`}
+          className='white-bg absolute x-remove elevation pointer-cursor text-medium'
+        >
           x
         </div>
       </div>
