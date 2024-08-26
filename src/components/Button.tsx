c814f5d62e527161e860496624feee6024e2c954
Andeskjerf <predgd@pm.me>
2024-08-26 10:58:22 +0200

    accept optional classname to button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,13 +4,14 @@ interface ButtonProps {
   label: string
   callback: Function
   active: boolean
+	className?: string
 }
 
-export default function Button({ label, callback, active }: ButtonProps) {
+export default function Button({ label, callback, active, className }: ButtonProps) {
   return (
     <div
       onClick={() => (active ? callback() : '')}
-      className={`${active ? 'button-active pointer-cursor' : ''} text-medium button br-button content-align-center no-select plr-24`}
+      className={`${active ? 'button-active pointer-cursor' : ''} ${className} text-medium button br-button content-align-center no-select plr-24`}
     >
       {label}
     </div>
