fcaccc0ec507c96ca73111314d0739c8bb1b0fe4
Andeskjerf <predgd@pm.me>
2024-09-13 10:36:25 +0200

    add unsubscribe page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,18 +1,31 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import App from './App'
 import { Journey } from './models/Journey'
 import SeatResults from './pages/SeatResults'
+import Unsubscribe from './pages/Unsubscribe'
 import BackButton from './components/BackButton'
 
 export enum Page {
   LANDING = 0,
   SEAT_RESULTS = 1,
+  UNSUBSCRIBE = 2,
 }
 
 export default function Router() {
   const [page, setPage] = useState<Page>(0)
   const refSelectedJourneys = useRef<Journey[]>()
   const showBack = useRef<boolean>(false)
+  const urlQuery = useRef<string>('')
+
+  useEffect(() => {
+    // get the path name, but strip away the / at the start
+    const pathName = window.location.pathname.slice(1)
+    urlQuery.current = window.location.search.slice(1)
+
+    if (pathName === 'unsubscribe') {
+      changePage(Page.UNSUBSCRIBE)
+    }
+  })
 
   function searchForSeats(selectedJourneys: Journey[]) {
     refSelectedJourneys.current = selectedJourneys
@@ -25,6 +38,7 @@ export default function Router() {
         showBack.current = false
         break
       case Page.SEAT_RESULTS:
+      case Page.UNSUBSCRIBE:
         showBack.current = true
         break
     }
@@ -32,6 +46,7 @@ export default function Router() {
   }
 
   function goBack() {
+    window.history.replaceState(null, '', '/')
     changePage(Page.LANDING)
   }
 
@@ -41,6 +56,9 @@ export default function Router() {
       pageToShow = (
         <SeatResults selectedJourneys={refSelectedJourneys.current} />
       )
+      break
+    case Page.UNSUBSCRIBE:
+      pageToShow = <Unsubscribe email={urlQuery.current}></Unsubscribe>
       break
     case Page.LANDING:
     default:
---
+++ src/pages/Unsubscribe.tsx
@@ -0,0 +1,67 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useEffect, useRef, useState } from 'react'
+import { isValidEmail } from '../utils/string'
+import {
+  faBan,
+  faCheck,
+  faGhost,
+  faPoo,
+} from '@fortawesome/free-solid-svg-icons'
+
+interface Props {
+  email: string
+}
+
+enum ValidType {
+  SUCCESS = 0,
+  NO_EMAIL = 1,
+  INVALID_EMAIL = 2,
+  BAD_REQUEST = 3,
+}
+
+export default function Unsubscribe({ email }: Props) {
+  const [isValid, setIsValid] = useState<ValidType>(0)
+  const splitEmail = useRef<string>('')
+
+  useEffect(() => {
+    let res = email.split('=')
+    if (res.length != 2 || res[0] != 'email') {
+      setIsValid(ValidType.BAD_REQUEST)
+    } else if (!isValidEmail(res[1])) {
+      setIsValid(ValidType.INVALID_EMAIL)
+    }
+    splitEmail.current = res[1]
+  })
+
+  const headerTexts = [
+    'Du er meldt ut',
+    'Ingen e-post funnet',
+    'Ugyldig e-post',
+    'Ugyldig URL',
+  ]
+  const icon = [faCheck, faGhost, faBan, faPoo]
+  const iconColor = ['green', 'black', 'red', 'black']
+  const paragraphTexts = [
+    `E-posten ${splitEmail.current} har blitt meldt ut fra fremtidige oppdateringer.`,
+    `E-posten du prøvde å melde ut (${splitEmail.current}) eksisterer ikke i våre systemer.`,
+    `E-posten du prøvde å melde ut (${splitEmail.current}) er ikke en gyldig e-post adresse.`,
+    'Linken du har brukt inneholder feil, ingenting er gjort.',
+  ]
+  const index = isValid
+
+  return (
+    <>
+      <div className='white-bg round-both elevation text-black p-18 email-confirmed-container text-align-start'>
+        <div className='flex'>
+          <FontAwesomeIcon
+            icon={icon[index]}
+            className={`${iconColor[index]} self-align-center p-18 heading drop-shadow`}
+          />
+          <div className='heading self-align-center'>{headerTexts[index]}</div>
+        </div>
+        <p>{paragraphTexts[index]}</p>
+        <p>Ta gjerne kontakt om du opplever andre problemer.</p>
+      </div>
+    </>
+  )
+}
