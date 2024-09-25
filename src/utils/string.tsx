e4adade68e1de0d84bb1ea60c1c271fea2ac5034
Andeskjerf <predgd@pm.me>
2024-09-25 13:16:12 +0200

    padNumber utils function

--- src/utils/string.tsx
+++ src/utils/string.tsx
@@ -7,3 +7,7 @@ export function isCharNumeric(text: string): boolean {
   if (text.length > 1) throw 'input is not a single character'
   return text >= '0' && text <= '9'
 }
+
+export function padNumber(text: string): string {
+  return String(text).padStart(2, '0')
+}
